887fdc1f0eea87f9c2643f2fabf5d098ad9be29d
Zarif25 <2502.zarif@gmail.com>
2025-07-17 12:40:16 +0600

    1.2 Implement minimal Lexer structure and token parsing logic

--- lexer/lexer.go
+++ lexer/lexer.go
@@ -3,25 +3,54 @@ package lexer
 import "lipi/token"
 
 type Lexer struct {
-	// TODO: Add fields to manage the input string, current position, and any
-	// other necessary state.
-
-	// For now, we just define an empty struct.
+	input []rune    // Input string as a slice of runes
+	position int    // Current position in the input string
+	ch rune         // Current character being examined
 }
 
 func NewLexer(input string) *Lexer {
-	// TODO: Initialize the lexer with the input string and set up any necessary
-	// state.
-
-	// For now, we just return a new Lexer instance without any state.
-	return &Lexer{}
+	runes := []rune(input)
+	return &Lexer{
+		input:    runes,
+		position: 0,
+		ch:       runes[0],
+	}
 }
 
 func (l *Lexer) NextToken() token.Token {
-	// TODO: Implement the logic to read the next token from the input string.
-	// This will involve skipping whitespace, identifying the type of the next token,
-	// and returning it.
-	
-	// For now, it just returns an illegal token.
-	return token.Token{Type: token.ILLEGAL, Literal: ""}
+	var tok token.Token
+	switch l.ch {
+	case '=':
+		tok = token.Token{Type: token.ASSIGN, Literal: string(l.ch)}
+	case '+':
+		tok = token.Token{Type: token.PLUS, Literal: string(l.ch)}
+	case ';':
+		tok = token.Token{Type: token.SEMICOLON, Literal: string(l.ch)}
+	case ',':
+		tok = token.Token{Type: token.COMMA, Literal: string(l.ch)}
+	case '(':
+		tok = token.Token{Type: token.L_PAREN, Literal: string(l.ch)}
+	case ')':
+		tok = token.Token{Type: token.R_PAREN, Literal: string(l.ch)}
+	case '{':
+		tok = token.Token{Type: token.L_BRACE, Literal: string(l.ch)}
+	case '}':
+		tok = token.Token{Type: token.R_BRACE, Literal: string(l.ch)}
+	default:
+		tok = token.Token{Type: token.ILLEGAL, Literal: string(l.ch)}
+	}
+	l.readChar()
+	return tok
+}
+
+// readChar advances the lexer to the next character in the input.
+// It increments the position and updates the current character (ch).
+// If the end of input is reached, ch is set to 0.
+func (l *Lexer) readChar() {
+	l.position++
+	if l.position >= len(l.input) {
+		l.ch = 0 // End of input
+	} else {
+		l.ch = l.input[l.position]
+	}
 }
